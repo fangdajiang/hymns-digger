4005edbc9c026d1df46e9b44929d1e728ceb1e70
fangdajiang <fang.dajiang@gmail.com>
2022-11-21 12:59:34 +0800

    read ES params from env

--- src/main/java/org/tlbc/hymns/config/NewRestClientConfig.java
+++ src/main/java/org/tlbc/hymns/config/NewRestClientConfig.java
@@ -1,16 +1,19 @@
 package org.tlbc.hymns.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 
 @Configuration
 public class NewRestClientConfig extends ElasticsearchConfiguration {
+    @Value("${elasticsearch.hostAndPort}")
+    private String hostAndPort;
 
     @Override
     public ClientConfiguration clientConfiguration() {
-        return ClientConfiguration.builder() //
-                .connectedTo("localhost:9200") //
+        return ClientConfiguration.builder()
+                .connectedTo(hostAndPort)
                 .build();
     }
 }
